0503575687c558c00c61eea8b6efa69af560fe0f
Tang Zhi <tgioer@163.com>
2021-12-19 10:53:20 +0800

    Update docs

--- h5/chunk/109.bf75c998.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[109],{"136":function(n,o,t){"use strict";t.r(o),t.d(o,"taro_movable_view_core",(function(){return c}));var r=t(32),c=function(){function e(n){Object(r.g)(this,n)}return e.prototype.componentDidLoad=function(){console.error("H5 暂不支持 MovableView 组件！")},e.prototype.render=function(){return Object(r.e)(r.a,null)},e}()}}]);
-//# sourceMappingURL=109.bf75c998.js.map
