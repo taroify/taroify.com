2b5c5ea4b53c14e89a472146ed5c144925b97e2b
Tang Zhi <tgioer@163.com>
2022-04-27 15:24:38 +0800

    Update docs

--- h5/chunk/109.7fd4d022.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[109],{"95":function(n,o,e){"use strict";e.r(o),e.d(o,"taro_match_media_core",(function(){return c}));var r=e(36),c=function(){function t(n){Object(r.g)(this,n)}return t.prototype.componentDidLoad=function(){console.error("H5 暂不支持 MatchMedia 组件！")},t.prototype.render=function(){return Object(r.e)(r.a,null)},t}()}}]);
-//# sourceMappingURL=109.7fd4d022.js.map
--- h5/chunk/110.08143dd0.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[110],{"96":function(n,o,r){"use strict";r.r(o),r.d(o,"taro_movable_area_core",(function(){return c}));var t=r(36),c=function(){function e(n){Object(t.g)(this,n)}return e.prototype.componentDidLoad=function(){console.error("H5 暂不支持 MovableArea 组件！")},e.prototype.render=function(){return Object(t.e)(t.a,null)},e}()}}]);
-//# sourceMappingURL=110.08143dd0.js.map
